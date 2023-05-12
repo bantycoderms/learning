f43d6f0bdf70f15a29a00a3cc9f28a975315981f
bantywithcode <bantydeniyel25@gmail.com>
2023-05-12 12:53:02 +0530

    adding message in javafile brother

--- java01.java
+++ java01.java
@@ -2,6 +2,6 @@ public class java01{
     public static void main(String[] args) {
         int a = 90;
         int b = 90;
-        System.out.println(a + b);
+        System.out.println(a + b + "this is message");
     }
 }
